161a4229ef4f91ab688724b21542a6de5b8ee159
Allen Yin <yinhaonan55@gmail.com>
2016-08-16 10:18:20 -0700

    jQuery project version 1

---
+++ JS_jQuery_project/JSquare.js
@@ -0,0 +1,42 @@
+$(document).ready(function  () {
+  for (i=0; i<16; i++){
+    $('tbody').append('<tr></tr');
+  }
+  for (i=0; i<16; i++){
+    $('tbody tr').append('<td><div></div></td>');
+  }
+  $('div').mouseenter(function(){
+    $(this).css('background-color', 'red');
+  });
+
+  $('div').mouseleave(function(){
+    $(this).css('background-color', 'blue');
+  });
+});
+
+function reset(){
+    var g=prompt('What should the side length (in squares) of the grid be?');
+    if ((isNaN(g))||(g==="")){
+        g=prompt('Sorry, that wasn\'t a number. What should the side length (in squares) of the grid be?');
+    }
+    $('tbody').empty();
+    for (i=0;i<g;i++) {
+        $('tbody').append('<tr></tr>');
+    }
+    for (i=0;i<g;i++) {
+        $('tbody tr').append('<td><div></div></td>');
+    }
+    $('td div').height(Math.ceil(320/g)+'px');
+    $('td div').width(Math.ceil(320/g)+'px');
+    $('td div').css('background-color','rgb(255,255,255)');
+    $('thead tr:first-child th').replaceWith('<th colspan="'+g+'"><h1>Let\'s create more</h1></th>');
+    $('thead tr:last-child th').replaceWith('<th colspan="'+g+'"><button type="button" onclick="reset()">Reset</button></th>');
+
+    $('div').mouseenter(function(){
+      $(this).css('background-color', 'red');
+    });
+
+    $('div').mouseleave(function(){
+      $(this).css('background-color', 'blue');
+    });
+}
